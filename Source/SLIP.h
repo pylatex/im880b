d31d32e7f23576d1567cc13bea40690771651834
Alex Bustos <alexfbp@gmail.com>
2017-06-27 19:38:31 -0500

    Moved source files to a folder

---
+++ Source/SLIP.h
@@ -0,0 +1,71 @@
+//------------------------------------------------------------------------------
+//
+//	File:		SLIP.h
+//
+//	Abstract:	SLIP Encode / Decoder
+//
+//	Version:	0.2
+//
+//	Date:		18.05.2016
+//
+//	Disclaimer:	This example code is provided by IMST GmbH on an "AS IS"
+//              basis without any warranties.
+//
+//------------------------------------------------------------------------------
+
+#ifndef SLIP_H
+#define SLIP_H
+
+//------------------------------------------------------------------------------
+//
+// Include Files
+//
+//------------------------------------------------------------------------------
+
+#include <stdint.h>
+
+//------------------------------------------------------------------------------
+//
+// General Definitions
+//
+//------------------------------------------------------------------------------
+
+// SLIP Protocol Characters
+#define SLIP_END					0xC0
+#define	SLIP_ESC					0xDB
+#define	SLIP_ESC_END				0xDC
+#define	SLIP_ESC_ESC				0xDD
+
+typedef uint8_t     UINT8;
+
+//------------------------------------------------------------------------------
+//
+//  Function Prototypes
+//
+//------------------------------------------------------------------------------
+
+// SLIP message receiver callback
+typedef UINT8*  (*TSLIP_CbRxMessage)(UINT8* message, int length);
+
+// Init SLIP layer
+void
+SLIP_Init(TSLIP_CbRxMessage cbRxMessage);
+
+// Init first receiver buffer
+bool
+SLIP_SetRxBuffer(UINT8* rxBuffer, int rxBufferSize);
+
+// Encode outgoing Data
+int
+SLIP_EncodeData(UINT8* dstBuffer, int txBufferSize, UINT8* srcData,int srcLength);
+
+// Decode incoming Data
+void
+SLIP_DecodeData(UINT8*  srcData, int srcLength);
+
+
+#endif // SLIP_H
+
+//------------------------------------------------------------------------------
+// end of file
+//------------------------------------------------------------------------------
---
+++ Source/SerialDevice.h
@@ -0,0 +1,50 @@
+#ifndef SERIAL_DEVICE_H
+#define SERIAL_DEVICE_H
+
+#include <stdint.h>
+
+#define Q_OS_WIN
+
+#ifdef  Q_OS_WIN
+
+#include <windows.h>
+
+#define Baudrate_9600       9600
+#define Baudrate_115200     115200
+#define DataBits_7          7
+#define DataBits_8          8
+#define Parity_Even         EVENPARITY
+#define Parity_None         NOPARITY
+
+#endif
+
+typedef uint8_t             UINT8;
+typedef uint32_t            UINT32;
+
+// open serial device
+bool
+SerialDevice_Open(const char*   comPort,
+                  UINT32        baudRate,
+                  int           dataBits,
+                  UINT8         parity);
+
+// close serial device
+bool
+SerialDevice_Close();
+
+// send single byte
+int
+SerialDevice_SendByte(UINT8 txByte);
+
+// send data
+int
+SerialDevice_SendData(UINT8*    txBuffer,
+                      int       txLength);
+
+// read data
+int
+SerialDevice_ReadData(UINT8*    rxBuffer,
+                      int       rxBufferSize);
+
+
+#endif // SERIAL_DEVICE_H
