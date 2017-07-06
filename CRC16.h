56821a449fd1065476e6b5a102a081ed7debcb8c
Alex Bustos <AlexFBP@users.noreply.github.com>
2017-07-06 17:09:05 -0500

    Librerias englobadas dentro del extern para C

--- CRC16.h
+++ CRC16.h
@@ -1,13 +1,9 @@
 //------------------------------------------------------------------------------
 //
 //	File:		CRC16.h
-//
 //	Abstract:	CRC16 calculation
-//
 //	Version:	0.2
-//
 //	Date:		18.05.2016
-//
 //	Disclaimer:	This example code is provided by IMST GmbH on an "AS IS"
 //              basis without any warranties.
 //
@@ -16,46 +12,45 @@
 #ifndef    __CRC16_H__
 #define    __CRC16_H__
 
-//------------------------------------------------------------------------------
-//
-//  Section Include Files
-//
-//------------------------------------------------------------------------------
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    //--------------------------------------------------------------------------
+    //  Section Include Files
+    //--------------------------------------------------------------------------
 
-#include <stdint.h>
-#include "globaldefs.h"
+    #include <stdint.h>
+    #include "globaldefs.h"
 
-//------------------------------------------------------------------------------
-//
-//  Section Defines & Declarations
-//
-//------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    //  Section Defines & Declarations
+    //--------------------------------------------------------------------------
 
-typedef uint8_t     UINT8;
-typedef uint16_t    UINT16;
+    typedef uint8_t     UINT8;
+    typedef uint16_t    UINT16;
 
-#define CRC16_INIT_VALUE    0xFFFF    // initial value for CRC algorithem
-#define CRC16_GOOD_VALUE    0x0F47    // constant compare value for check
-#define CRC16_POLYNOM       0x8408    // 16-BIT CRC CCITT POLYNOM
+    #define CRC16_INIT_VALUE    0xFFFF    // initial value for CRC algorithem
+    #define CRC16_GOOD_VALUE    0x0F47    // constant compare value for check
+    #define CRC16_POLYNOM       0x8408    // 16-BIT CRC CCITT POLYNOM
 
-//------------------------------------------------------------------------------
-//
-//  Function Prototypes
-//
-//------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    //  Function Prototypes
+    //--------------------------------------------------------------------------
 
-// Calc CRC16
-UINT16
-CRC16_Calc  (UINT8*     data,
-             UINT16     length,
-             UINT16     initVal);
+    // Calc CRC16
+    UINT16
+    CRC16_Calc  (UINT8*     data,
+                 UINT16     length,
+                 UINT16     initVal);
 
-// Calc & Check CRC16
-bool
-CRC16_Check (UINT8*     data,
-             UINT16     length,
-             UINT16     initVal);
-
+    // Calc & Check CRC16
+    bool
+    CRC16_Check (UINT8*     data,
+                 UINT16     length,
+                 UINT16     initVal);
+#ifdef	__cplusplus
+}
+#endif
 
 #endif // __CRC16_H__
 //------------------------------------------------------------------------------
--- SerialDevice.h
+++ SerialDevice.h
@@ -1,58 +1,64 @@
 #ifndef SERIAL_DEVICE_H
 #define SERIAL_DEVICE_H
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "globaldefs.h"
-
-#if defined Q_OS_WIN
-#include <windows.h>
-
-#define Baudrate_9600       9600
-#define Baudrate_115200     115200
-#define DataBits_7          7
-#define DataBits_8          8
-#define Parity_Even         EVENPARITY
-#define Parity_None         NOPARITY
-
-#elif defined UC_PIC8
-
-#define Baudrate_9600       9600
-#define Baudrate_115200     115200
-#define DataBits_7          7
-#define DataBits_8          8
-//#define Parity_Even         EVENPARITY
-//#define Parity_None         NOPARITY
-
+#ifdef	__cplusplus
+extern "C" {
 #endif
+    #include <stdint.h>
+    #include <stdbool.h>
+    #include "globaldefs.h"
 
-typedef uint8_t             UINT8;
-typedef uint32_t            UINT32;
+    #if defined Q_OS_WIN
+    #include <windows.h>
 
-// open serial device
-bool
-SerialDevice_Open(const char*   comPort,
-                  UINT32        baudRate,
-                  int           dataBits,
-                  UINT8         parity);
+    #define Baudrate_9600       9600
+    #define Baudrate_115200     115200
+    #define DataBits_7          7
+    #define DataBits_8          8
+    #define Parity_Even         EVENPARITY
+    #define Parity_None         NOPARITY
 
-// close serial device
-bool
-SerialDevice_Close();
+    #elif defined UC_PIC8
 
-// send single byte
-int
-SerialDevice_SendByte(UINT8 txByte);
+    #define Baudrate_9600       9600
+    #define Baudrate_115200     115200
+    #define DataBits_7          7
+    #define DataBits_8          8
+    //#define Parity_Even         EVENPARITY
+    //#define Parity_None         NOPARITY
 
-// send data
-int
-SerialDevice_SendData(UINT8*    txBuffer,
-                      int       txLength);
+    #endif
 
-// read data
-int
-SerialDevice_ReadData(UINT8*    rxBuffer,
-                      int       rxBufferSize);
+    typedef uint8_t             UINT8;
+    typedef uint32_t            UINT32;
 
+    // open serial device
+    bool
+    SerialDevice_Open(const char*   comPort,
+                      UINT32        baudRate,
+                      int           dataBits,
+                      UINT8         parity);
+
+    // close serial device
+    bool
+    SerialDevice_Close();
+
+    // send single byte
+    int
+    SerialDevice_SendByte(UINT8 txByte);
+
+    // send data
+    int
+    SerialDevice_SendData(UINT8*    txBuffer,
+                          int       txLength);
+
+    // read data
+    int
+    SerialDevice_ReadData(UINT8*    rxBuffer,
+                          int       rxBufferSize);
+    
+#ifdef	__cplusplus
+}
+#endif
 
 #endif // SERIAL_DEVICE_H
