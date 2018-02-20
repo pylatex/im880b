7d29e919dedfc238bae9508dc82ff4164273120a
Alex Bustos <AlexFBP@users.noreply.github.com>
2018-02-20 11:32:42 -0500

    Separacion inicial de la maquina de estados

--- src/pylatex.c
+++ src/pylatex.c
@@ -19,6 +19,17 @@
 #define LARGO   20
 #define PUERTO  5
 
+static enum {
+    RESET,
+    TestUART,
+    GetNwkStatus,
+    NWKinactive,
+    NWKjoining,
+    NODEidleActive,
+    NWKaccept,
+    SENSprocess
+} status = RESET; //Initial State for the machine of Main.
+
 typedef struct {
     char carga[LARGO];
     char cnt;
@@ -27,6 +38,7 @@ typedef struct {
 #define initPayload() PY.cnt=0
 
 static PY_T PY;
+volatile static HCIMessage_t RxMessage;
 
 //------------------------------------------------------------------------------
 //  Function Implementations
@@ -67,4 +79,124 @@ char *short2charp (unsigned short in) {
     val[0]=in>>8    & 0xFF;
     val[1]=in       & 0xFF;
     return val;
+}
+
+void maquina () {
+    switch (status) {
+        case RESET:
+            pendingmsg = false;
+            timeouts = 0;
+            if (InitHCI(ProcesaHCI,(HCIMessage_t *) &RxMessage))
+                status = TestUART; //Full Duplex UART and Rx interruptions enabled
+            break;
+        case TestUART:
+            WiMOD_LoRaWAN_SendPing();
+            //Wait for TimeOut or HCI message and identify the situation:
+            StartTimerDelayMs(20);
+            while (TMR1ON && !pendingmsg); //Escapes at timeout or HCI received.
+            if (pendingmsg) {
+                timeouts=0;
+                if ((RxMessage.SapID == DEVMGMT_ID) && (RxMessage.MsgID == DEVMGMT_MSG_PING_RSP)) {
+                    //The incoming HCI message is a response of PING
+                    status = GetNwkStatus;
+                    pendingmsg = false;
+                }
+            }
+            break;
+        case GetNwkStatus:
+            WiMOD_LoRaWAN_GetNetworkStatus();
+            //Wait for TimeOut or HCI message and identify the situation:
+            StartTimerDelayMs(20);
+            while (TMR1ON && !pendingmsg); //Escapes at timeout or HCI received.
+            if (pendingmsg) {
+                timeouts=0;
+                if ((RxMessage.SapID == LORAWAN_ID) && (RxMessage.MsgID == LORAWAN_MSG_GET_NWK_STATUS_RSP)) {
+                    pendingmsg = false;
+                    //The incoming HCI message is a response of NWK STATUS
+                    switch (RxMessage.Payload[1]) {
+                        case 0: //Inactive
+                        case 1: //Active (ABP)
+                            status = NWKinactive;
+                            break;
+                        case 2: //Active (OTAA)
+                            status = NODEidleActive;
+                            break;
+                        case 3: //Accediendo (OTAA)
+                            status = NWKjoining;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            break;
+        case NWKinactive:
+            WiMOD_LoRaWAN_JoinNetworkRequest();
+            //Wait for TimeOut or HCI message and identify the situation:
+            StartTimerDelayMs(20);
+            while (TMR1ON && !pendingmsg); //Escapes at timeout or HCI received.
+            if (pendingmsg) {
+                timeouts=0;
+                if ((RxMessage.SapID == LORAWAN_ID) && (RxMessage.MsgID == LORAWAN_MSG_JOIN_NETWORK_RSP)) {
+                    //The incoming HCI message is a response of NWK STATUS
+                    if (RxMessage.Payload[0] == LORAWAN_STATUS_OK) {
+                        pendingmsg = false;
+                        status = NWKjoining;
+                    }
+                }
+            }
+            break;
+        case NWKjoining:
+            //Wait for TimeOut or HCI message and identify the situation:
+            StartTimerDelayMs(20);
+            while (TMR1ON && !pendingmsg); //Escapes at timeout or HCI received.
+            if (pendingmsg) {
+                timeouts=0;
+                if ((RxMessage.SapID == LORAWAN_ID) && (RxMessage.MsgID == LORAWAN_MSG_JOIN_NETWORK_IND)) {
+                    //The incoming HCI message is a join event
+                    switch (RxMessage.Payload[0]) {
+                        case 0x00:  //device successfully activated
+                        case 0x01:  //device successfully activated, Rx Channel Info attached
+                            status = NWKaccept;
+                        default:
+                            break;
+                    }
+                }
+            }
+            break;
+        case NWKaccept:
+            //Wait for TimeOut or HCI message and identify the situation:
+            StartTimerDelayMs(20);
+            while (TMR1ON && !pendingmsg); //Escapes at timeout or HCI received.
+            if (pendingmsg) {
+                timeouts=0;
+                if ((RxMessage.SapID == LORAWAN_ID) && (RxMessage.MsgID == LORAWAN_MSG_RECV_NO_DATA_IND)) {
+                    //The incoming HCI message is an indication of reception with no data
+                    if (RxMessage.Payload[0] == LORAWAN_STATUS_OK) {
+                        pendingmsg = false;
+                        status = NODEidleActive;
+                    }
+                }
+            }
+            break;
+        case NODEidleActive:
+            //Create more states to attend other HCI messages or modify this state
+            ms100(49);//Espera ~5 segundos
+            status = SENSprocess;
+            break;
+        case SENSprocess:
+            LED=true;
+            //Starts an I2C reading and decides upon the response.
+            respuesta=T67xx_C02();
+            if (respuesta)
+                AppendMeasure(PY_CO2,respuesta);
+            AppendMeasure(PY_GAS,short2charp(valorPropano()));
+            SendMeasures();
+            status = NODEidleActive;
+            ms100(1);   //Completa los 5 segundos...
+            LED=false;
+            break;
+        default:
+            break;
+    }
 }
