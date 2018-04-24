8a56e2db7eb91d7e0be78cfe4b36c26a8a79ff91
Alex Bustos <AlexFBP@users.noreply.github.com>
2018-04-24 15:19:53 -0500

    GPS: Librería reescrita

--- src/nmea.c
+++ src/nmea.c
@@ -2,71 +2,134 @@
 
 #define PTR_SZ  20
 
-typedef struct {
-    char         *pers[PTR_SZ]; //Indices en que se encuentran ubicadas las comas
-    unsigned char fields;       //Cantidad de comas
-    unsigned int  CSgiven;      //Suma de verificacion (parseado de los Ultimos dos octetos)
-    unsigned char CScalc;       //Suma de verificacion (XOR entre $ y *)
+volatile static char    pers[PTR_SZ];   //Indices en que se encuentran ubicadas las comas
+
+typedef enum {
+    WaitingStart,Receiving,WaitingCheckHigh,WaitingCheckLow,WaitingReading,BufferFull,ChecksumError
+} NMEA_fsm_stat_t;
+
+typedef volatile struct {
+    char           *buffer;
+    char            buffsize;
+    char            fields;       //Cantidad de comas
+    unsigned char   CSgiven;      //Suma de verificacion (parseado de los Ultimos dos octetos)
+    unsigned char   CScalc;       //Suma de verificacion (XOR entre $ y *, sin incluirlos)
+    NMEA_fsm_stat_t stat;
+    NMEAstatus     *UserReg;
 } NMEA_t;
 
 static NMEA_t NMEA;
-unsigned char NibbleVal (unsigned char in);
 
-char NMEAload (char *phrase, unsigned char len) {
-    unsigned char aux=0;
+char NibbleVal (char in);
 
-    //Detect and skip the beginning of NMEA content
-    while (len) {
-        if (*phrase=='$' || *phrase=='!') {
-            if (!aux) aux=1;
-        } else {
-            if (aux) break;
-        }
+void NMEAinit (char *RxBuffer,char RxBufferSize,NMEAstatus *statusReg) {
+    NMEA.buffer=RxBuffer;
+    NMEA.buffsize=RxBufferSize;
+    NMEA.UserReg=statusReg;
+    NMEArelease();
+    pers[0]=0;
+}
 
+char NMEAload (const char *phrase){
+    NMEArelease();
+    while (*phrase) {
+        NMEAinput(*phrase);
         phrase++;
-        len--;
     }
-    NMEA.CScalc=0;
-    NMEA.fields=0;
-    while (len && NMEA.fields<PTR_SZ) {
-        if (aux==2) {
-            //End of NMEA sentence
-            NMEA.CSgiven=(NibbleVal(*phrase) << 4) + NibbleVal(*(phrase+1));
-            break;
-        } else {
-            NMEA.CScalc ^= *phrase;
-            if (aux)
-                NMEA.pers[NMEA.fields++]=phrase;
-            switch (*phrase) {
-                case ',':
-                    aux=1;
-                    break;
-                case '*':
-                    aux=2;
-                    NMEA.CScalc ^= '*'; //Restaurar el checksum del asterisco.
-                    break;
-                default:
-                    aux=0;
-                    break;
-            }
-            if (aux)
-                *phrase=0;
-        }
-        phrase++;
-        len--;
-    }
-
     return NMEA.fields;
 }
 
+void NMEAinput (char incomingByte) {
+    static char count=0;
+    static enum {none,cash,exclam} type=none;
+
+    switch (NMEA.stat) {
+
+        case WaitingStart:
+            if (incomingByte=='$' || incomingByte=='!'){
+                if (incomingByte=='$')
+                    type=cash;
+                else
+                    type=exclam;
+                NMEA.stat = Receiving;
+                count=0;
+            }
+            break;
+
+        case Receiving:
+            if (incomingByte=='*') {
+                NMEA.stat = WaitingCheckHigh;
+                NMEA.buffer[count] = 0;
+            } else if (incomingByte!='$' || incomingByte!='!') {
+                NMEA.CScalc ^= incomingByte;
+                if (incomingByte==','){
+                    NMEA.buffer[count] = 0;
+                    pers[NMEA.fields++] = count+1;
+                } else {
+                    NMEA.buffer[count] = incomingByte;
+                }
+                count++;
+                if (count==NMEA.buffsize) {
+                    NMEA.stat = BufferFull;
+                    NMEA.UserReg->full = 1;
+                }
+            }
+            break;
+
+        case BufferFull:
+            if (incomingByte=='*') {
+                NMEA.stat = WaitingCheckHigh;
+            } else {
+            }
+            break;
+
+        case WaitingCheckHigh:
+            count = NibbleVal(incomingByte);    //Variable recycled
+            if (count & 0xF0) {
+                //Error
+                NMEA.stat = ChecksumError;
+            } else {
+                NMEA.stat = WaitingCheckLow;
+                NMEA.CSgiven = (unsigned char)((count << 4) | (count >> 4)); //would be optimized to SWAPF
+            }
+            break;
+
+        case WaitingCheckLow:
+            count = NibbleVal(incomingByte);    //Variable recycled
+            if (count & 0xF0) {
+                //Error
+                NMEA.stat = ChecksumError;
+            } else {
+                NMEA.stat = WaitingReading;
+                NMEA.CSgiven += count;
+                NMEA.UserReg->checksumErr= (NMEA.CSgiven == NMEA.CScalc)?0u:1u;
+                NMEA.UserReg->isCash=type == cash;
+                NMEA.UserReg->isExclam=type == exclam;
+                NMEA.UserReg->complete=1;
+                type = none;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
 char *NMEAselect (unsigned char item) {
-    if (item <= NMEA.fields)
-        return NMEA.pers[item];
+    if (item < NMEA.fields)
+        return NMEA.buffer + pers[item];
     else
         return 0;
 }
 
-unsigned char NibbleVal (unsigned char in) {
+void NMEArelease (void) {
+    NMEA.UserReg->reg=0;
+    NMEA.stat=WaitingStart;
+    NMEA.fields=1;
+    NMEA.CScalc=0;
+}
+
+char NibbleVal (char in) {
     if (in >= '0' && in <= '9')
         return in-'0';
     else if (in >= 'A' && in <= 'F')
--- src/nmea.h
+++ src/nmea.h
@@ -15,8 +15,49 @@
 extern "C" {
 #endif
 
-char NMEAload (char *phrase, unsigned char len);
-char *NMEAselect (unsigned char item);
+    typedef volatile union {
+        char reg;
+        struct {
+            unsigned    full            :1; //The buffer is full
+            unsigned    checksumErr     :1; //Invalid checksum (given or calculated)
+            unsigned    complete        :1; //Fully recognized message, ready to be read.
+            unsigned    isCash          :1; //NMEA message begun with '$'
+            unsigned    isExclam        :1; //NMEA message begun with '!'
+        };
+    } NMEAstatus;
+
+    /**
+     * Configures the NMEA decoder.
+     * @param RxBuffer      Reception buffer.
+     * @param RxBufferSize  Size of reception buffer.
+     * @param statusReg     A status register the user can read.
+     */
+    void NMEAinit (char *RxBuffer,char RxBufferSize,NMEAstatus *statusReg);
+
+    /**
+     * Parses a NMEA message. Calls NMEAload() for every byte on the message.
+     * @param phrase A pointer to the message to be read
+     * @return The number of fields detected
+     */
+    char NMEAload (const char *phrase);
+
+    /**
+     * Input a char to the internal NMEA FSM.
+     * @param incomingByte
+     */
+    void NMEAinput (char incomingByte);
+
+    /**
+     * Gives the position of a specified field
+     * @param item: the field to be read, starting on 0
+     * @return: first byte of the field, null if not a valid position.
+     */
+    char *NMEAselect (unsigned char item);
+
+    /**
+     * Allows to decode a new message.
+     */
+    void NMEArelease (void);
 
 #ifdef	__cplusplus
 }
