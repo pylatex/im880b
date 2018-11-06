38a750bd83f6392f87b045e3881bfed986f204b0
Alex Bustos <AlexFBP@users.noreply.github.com>
2018-11-06 15:39:14 -0500

    Agregados pull-up a los pines I2C

--- src/nucleoPIC.c
+++ src/nucleoPIC.c
@@ -6,35 +6,23 @@
 void setup (void) {
 
     //OSCILLATOR
-#ifdef _18F2550
-    OSCCON=0x73;    //Internal at 8 MHz
-    while(!IOFS);   //Waits for stabilization
-#endif
-#ifdef _16F1769
     OSCCON=0x70;    //Internal at 8 MHz
     while(!HFIOFS); //May be either PLLR or HFIOFS. See OSCSTAT register for specific case
-#endif
 
     //PINS SETUP
-#ifdef _16F1769
     //PORTC=0;
     //LATC=0;
     ANSELA=0;       //All pins as digital
     ANSELB=0;       //All pins as digital
     ANSELC=0;       //All pins as digital
     ODCONCbits.ODC1 = true;    //Enable Open Drain to drive DVI
-    TRISC=0x7C; //RC0 and RC1 as outputs
-#endif
-#ifdef _18F2550
-    PORTA=0;
-    LATA=0;
-    ADCON1=0x0E;
-    ADCON2=0x83;
-    TRISA=0xFD; //RA1 as output
-#endif
+    TRISC=0x7C; //RC0/1/7 as outputs
+
+    OPTION_REGbits.nWPUEN=0;
+    WPUBbits.WPUB4=1;
+    WPUBbits.WPUB6=1;
 
     //UART & I2C
-#ifdef _16F1769
 
     //Entradas y salidas I2C se dejan en los pines por defecto: SCK:RB6, SDA:RB4
     //(unicos totalmente compatibles con I2C/SMBus, segun seccion 12.3 del datasheet)
@@ -44,7 +32,6 @@ void setup (void) {
     SSPCLKPPS = 0x0E;   //RB6->MSSP:SCL;
 
     ppsLock(true);
-#endif
 
     __delay_ms(200);   //Delay for stabilization of power supply
 
