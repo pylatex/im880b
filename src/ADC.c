a11b24bf1d1476cb55f3bd38f60f1c16543a7c2c
Juan Jose <juanjose58689@hotmail.com>
2019-03-18 16:34:41 -0500

    Habilitar y configurar ADC

--- src/ADC.c
+++ src/ADC.c
@@ -1,20 +1,109 @@
 #include <xc.h>
+#include <math.h>
 #include "ADC.h"
-
-typedef struct {
-    unsigned short adcres;
-} MQ2_t;
-
-MQ2_t MQ2;
-
-
-void ADCpropano(void) {
-    ADCON0=0x03;    //Inicia Medicion en AD0
+void Config_ADC(void){
+    //configuracion de los pines A0,A1,A2,C0 como entradas
+    TRISAbits.TRISA0=0;
+    TRISAbits.TRISA1=0;
+    TRISAbits.TRISA2=0;    
+    TRISCbits.TRISC0=0;
+    
+    //configurando los pines A0,A1,A2,C0 como entradas analogas
+    ANSELAbits.ANSA0=1;
+    ANSELAbits.ANSA1=1;
+    ANSELAbits.ANSA2=1;
+    ANSELCbits.ANSC0=1;
+    //Desactivar los pull_up 
+    WPUAbits.WPUA0=1;
+    WPUAbits.WPUA1=1;
+    WPUAbits.WPUA2=1;
+    WPUCbits.WPUC0=1;
+    
+    //CONFIGURACION DEL PERIFERICO ADC
+    ADCON1bits.ADFM=1;      //justificado a la derecha
+    ADCON1bits.ADCS=3;      //FOSC/32 tiempo recomendado de TAD para trabajor a los 8MHz
+    ADCON1bits.ADNREF=0;    //VREF- is connected to VSS    
+    ADCON1bits.ADPREF=0;    //VREF+ is connected to VDD
+    ADCON0bits.ADON=0;      //periferico  apagado, para ahorrar energia
+    
+    
+            
+    
+    
+}
+float valorPropano(void) {
+    
+    //convercion ADC 
+    
+    ADCON0bits.CHS=0;       //seleccion de canal 0
+    ADCON0bits.ADON=1;      //encender el periferico ADC
+    __delay_us(10);          //espera requerida por el ADC
+    ADCON0bits.GO=1;        //Inicia Medicion en AD0    
     while(ADCON0bits.GO);      //Se queda esperando hasta que acabe la conversion
-    MQ2.adcres = ADRES;
+    float R_adc=ADRES;
+    ADCON0bits.ADON=0;      //apagar el periferico ADC para ahorrar energia
+    
+    //escalamiento de las unidades
+    float Voltaje=R_adc*5/1024;
+    float Rs=1000*((5-Voltaje)/Voltaje);
+//    float ppm=0.4091*pow(Rs/5463,-1.497);
+    return Rs;
 }
 
-unsigned short valorPropano(void) {
-    ADCpropano();
-    return MQ2.adcres;
+float valorozono(void) {
+    
+    //convercion ADC 
+    
+    ADCON0bits.CHS=1;       //seleccion de canal 1
+    ADCON0bits.ADON=1;      //encender el periferico ADC
+    __delay_us(10);          //espera requerida por el ADC
+    ADCON0bits.GO=1;        //Inicia Medicion en AD1  
+    while(ADCON0bits.GO);      //Se queda esperando hasta que acabe la conversion
+    float R_adc=ADRES;    
+    ADCON0bits.ADON=0;      //apagar el periferico ADC para ahorrar energia
+    
+    //escalamiento de las unidades
+    float Voltaje=R_adc*5/1024;
+    float Rs=1000*((5-Voltaje)/Voltaje);
+//    float ppm=0.4091*pow(Rs/5463,-1.497);
+    return Rs;
+}
+
+float valorCO(void) {
+    
+    //convercion ADC 
+    
+    ADCON0bits.CHS=2;       //seleccion de canal 2
+    ADCON0bits.ADON=1;      //encender el periferico ADC
+    __delay_us(10);          //espera requerida por el ADC
+    ADCON0bits.GO=1;        //Inicia Medicion en AD2  
+    while(ADCON0bits.GO);      //Se queda esperando hasta que acabe la conversion
+    float R_adc=ADRES;
+    ADCON0bits.ADON=0;      //apagar el periferico ADC para ahorrar energia
+    
+    //escalamiento de las unidades
+    float Voltaje=R_adc*5/1024;
+    float Rs=1000*((5-Voltaje)/Voltaje);
+//    float ppm=0.4091*pow(Rs/5463,-5);
+    return Rs;
+}
+
+
+float valor135(void) {
+    
+    //convercion ADC 
+        
+    ADCON0bits.CHS=4;       //seleccion de canal 4
+    ADCON0bits.ADON=1;      //encender el periferico ADC
+    __delay_us(10);          //espera requerida por el ADC
+    ADCON0bits.GO=1;        //Inicia Medicion en AD4  
+    while(ADCON0bits.GO);      //Se queda esperando hasta que acabe la conversion
+    float R_adc=ADRES;
+    ADCON0bits.ADON=0;      //apagar el periferico ADC para ahorrar energia
+    
+    //escalamiento de las unidades
+    float Voltaje=R_adc*5/1024;
+    float Rs=1000*((5-Voltaje)/Voltaje);
+//    float ppm=0.4091*pow(Rs/5463,-5);
+    return Rs;
 }
--- src/examples/main_pic8.c
+++ src/examples/main_pic8.c
@@ -8,10 +8,10 @@
  */
 
 //MODOS DE COMPILACION. Descomentar el que se quiera probar:
-#define SMACH       //Maquina de estados (principal)
+//#define SMACH       //Maquina de estados (principal)
 //#define TEST_1      //Verificacion UART/Reloj
 //#define TEST_2      //Verificacion comunicacion PIC-WiMOD
-//#define TEST_4      //Medicion ADC y envio por UART
+#define TEST_4      //Medicion ADC y envio por UART
 
 //------------------------------------------------------------------------------
 //  Definitions and Setup
@@ -76,7 +76,9 @@ void main(void)
     #ifndef TEST_4
     enableInterrupts();
     #endif
-
+    #ifdef ADC_H
+    Config_ADC();
+    #endif
     #ifdef HDC1010_H
     unsigned short temp,hum;
     HDCinit (&temp,&hum,msdelay);
@@ -191,10 +193,13 @@ void main(void)
 
         //Prueba 4: Medicion ADC y envio por UART
         #ifdef TEST_4
-        unsigned char phrase[15],phlen;
-        phlen=sprintf(phrase,"Propano:%u\r\n",valorPropano());
-        SerialDevice_SendData(phrase,phlen);
-        ms100(10);
+        float aa=valorozono(),bb=valor135(),cc=valorCO();
+        
+        ////        phlen=sprintf(phrase,"Propano:%u\r\nOzono:%u\r\nCalidad del Aire:%u\r\nMonoxido de Carbono:%u\r\n\n\n",valorPropano(),valorozono(),valor135(),valorCO());
+        printf("%f\n%f\n",aa,bb);
+//        phlen=sprintf(phrase,"Ozono:%f\r\nCalidad del Aire:%f\r\nMonoxido de Carbono:%f\r\n\n\n",aa,bb,cc);
+//        SerialDevice_SendData(phrase,phlen);
+//        ms100(10);
         #endif
     }
 }
